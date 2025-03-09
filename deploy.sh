4e43eed0a348d75bf32b35b188e499d1d880257a
Mads Ohm Larsen <mads.ohm@gmail.com>
2025-03-09 16:10:52 +0100

    Simplified wishlist

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ npm run build
 
 # Copy the build folder to the server
 scp build/index.html ohmbrix.dk:public_html/ønsker.html
-scp -r build ohmbrix.dk:public_html/.
+scp -r build/* ohmbrix.dk:public_html/.
 scp index.html ohmbrix.dk:public_html/.
